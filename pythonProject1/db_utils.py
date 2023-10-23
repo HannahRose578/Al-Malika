9c9998e5dbe6db4cdd3ce9ab929dbc9cdd1eaea4
Hannah Rose <52452654+HannahRose578@users.noreply.github.com>
2023-10-23 14:15:22 +0100

    Update db_utils.py

--- pythonProject1/db_utils.py
+++ pythonProject1/db_utils.py
@@ -1,40 +1,15 @@
-import pymysql
-from pymysql.cursors import DictCursor
+import mysql.connector
 from config import DATABASE_CONFIG
 
-
 def connect_to_db():
-    try:
-        connection = pymysql.connect(**DATABASE_CONFIG)
-        return connection
-    except pymysql.MySQLError as e:
-        print(f"Error while connecting to MySQL: {e}")
-        return None
-
+    return mysql.connector.connect(**DATABASE_CONFIG)
 
 def execute_query(connection, query, params=None):
-    if connection is None:
-        print("Connection is not established.")
-        return None
-
-    try:
-        with connection.cursor(DictCursor) as cursor:
-            cursor.execute(query, params)
-            result = cursor.fetchall()
-        return result
-    except pymysql.MySQLError as e:
-        print(f"Error while executing query: {e}")
-        return None
-
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute(query, params)
+    result = cursor.fetchall()
+    cursor.close()
+    return result
 
 def close_connection(connection):
-    if connection:
-        connection.close()
-
-
-# Example usage
-# conn = connect_to_db()
-# if conn:
-#     result = execute_query(conn, "SELECT * FROM some_table")
-#     print(result)
-#     close_connection(conn)
+    connection.close()
