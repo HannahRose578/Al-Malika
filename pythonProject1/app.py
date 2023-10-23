522e2f18df748486f5565ba606fc73c253763a8b
Hannah Rose <52452654+HannahRose578@users.noreply.github.com>
2023-10-23 22:20:41 +0100

    Update app.py

--- pythonProject1/app.py
+++ pythonProject1/app.py
@@ -1,57 +1,45 @@
 from flask import Flask, jsonify, request
-from db_utils import connect_to_db, execute_query, close_connection
+import db_utils
 
 app = Flask(__name__)
 
-# Return all books
 @app.route('/books', methods=['GET'])
 def get_books():
-    connection = connect_to_db()
-    query = "SELECT * FROM Books"
-    books = execute_query(connection, query)
-    close_connection(connection)
+    connection = db_utils.get_db_connection()
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM books")
+    books = cursor.fetchall()
+    connection.close()
     return jsonify(books)
 
-# Get books by genre
-@app.route('/books/genre/<genre>', methods=['GET'])
-def get_books_by_genre(genre):
-    connection = connect_to_db()
-    query = "SELECT * FROM Books WHERE genre = %s"
-    params = (genre,)
-    books = execute_query(connection, query, params)
-    close_connection(connection)
-    return jsonify(books)
-
-# Return Authors beginning with A
-@app.route('/books/author', methods=['GET'])
-def get_author_A():
-    connection = connect_to_db()
-    query = "SELECT * FROM Books WHERE author LIKE 'a%'"
-    books = execute_query(connection, query)
-    close_connection(connection)
-    return jsonify(books)
-
-# Adding a new book
 @app.route('/books', methods=['POST'])
 def add_book():
-    new_book = request.get_json()
-    connection = connect_to_db()
-    query = "INSERT INTO Books (title, author, publication_year, isbn, genre) VALUES (%s, %s, %s, %s, %s)"
-    params = (new_book['title'], new_book['author'], new_book['publication_year'], new_book['isbn'], new_book['genre'])
-    execute_query(connection, query, params)
-    close_connection(connection)
-    return jsonify(new_book)
+    new_book = request.json
+    connection = db_utils.get_db_connection()
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO books (title, author) VALUES (%s, %s)", (new_book['title'], new_book['author']))
+    connection.commit()
+    connection.close()
+    return jsonify(new_book), 201
 
-# Updating the genre of a book
-@app.route('/books/<isbn>', methods=['PUT'])
-def update_genre(isbn):
-    updated_genre = request.get_json()
-    connection = connect_to_db()
-    query = "UPDATE Books SET genre = %s WHERE isbn = %s"
-    params = (updated_genre['genre'], isbn)
-    execute_query(connection, query, params)
-    close_connection(connection)
-    return jsonify(updated_genre)
+@app.route('/books/<int:book_id>', methods=['PUT'])
+def update_book(book_id):
+    updated_book = request.json
+    connection = db_utils.get_db_connection()
+    cursor = connection.cursor()
+    cursor.execute("UPDATE books SET title=%s, author=%s WHERE id=%s", (updated_book['title'], updated_book['author'], book_id))
+    connection.commit()
+    connection.close()
+    return jsonify(updated_book)
+
+@app.route('/random_book', methods=['GET'])
+def get_random_book():
+    connection = db_utils.get_db_connection()
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM books ORDER BY RAND() LIMIT 1")
+    book = cursor.fetchone()
+    connection.close()
+    return jsonify(book)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True)
